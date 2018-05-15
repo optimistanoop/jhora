afdc63cff3b205dbc5ae2a40fd7639e358e38f7a
khushboo <khushboovjvrg018@gmail.com>
2018-05-15 23:46:19 +0530

    db.js

--- app/db.js
+++ app/db.js
@@ -1,17 +1,14 @@
-let Query = require('./query.js');
+et Query = require('./query.js');
 let sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('/Users/anoop/Documents/electron-boilerplate-sqlite/db.db');
 let q = new Query(db);
 let ipcRenderer = require('electron').ipcRenderer;
 
 ipcRenderer.on('close-db', (event, message) => {
-  console.log('anp going to close the db', message);
-  db.close();
-  ipcRenderer.send('closed-db', 'thanks');
+ console.log('anp going to close the db', message);
+ db.close();
+ ipcRenderer.send('closed-db', 'thanks');
 });
-
-
-
 
 q.createCustomerTable();
 q.createTransectionTable();
