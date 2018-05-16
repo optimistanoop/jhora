127a6ea1a64d42e61028250b1e4dbd71532c7d5e
optimistanoop <optimistanoop@gmail.com>
2018-05-16 19:20:48 +0530

    first selected tab

--- app/jhora.js
+++ app/jhora.js
@@ -4,15 +4,15 @@ const {dialog} = require('electron').remote
 let jhora = angular.module('jhora', ['ngMaterial', 'ngMessages']);
 jhora.controller('jhoraCtrl', function($scope) {
   $scope.villages = ['Daniyari', 'Gadahia Mohan', 'Koindaha'];
-  $scope.selectedIndex = 0;
+  $scope.currentNavItem = '0';
   $scope.tabs = [{title:'Customer', content:'customer/customer.html'}, 
                  {title:'View Customer', content:'customer/customerView.html'},
                  {title:'Transection', content:'transection/transection.html'},
                  {title:'View Transection', content:'transection/transectionView.html'}
                ];
-  $scope.tab = $scope.tabs[0].content;
+  $scope.template = $scope.tabs[0].content;
   $scope.goto = function(page) {
-    $scope.tab = $scope.tabs[page].content;
+    $scope.template = $scope.tabs[page].content;
   };             
 })
 .config(function($mdThemingProvider) {
