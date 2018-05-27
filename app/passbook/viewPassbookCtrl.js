5a4891774026e8cfa1704bca6c2fe94f4bb6d85b
optimistanoop <optimistanoop@gmail.com>
2018-05-27 11:29:33 +0530

    edit del in passbook

--- app/passbook/viewPassbookCtrl.js
+++ app/passbook/viewPassbookCtrl.js
@@ -8,6 +8,39 @@ jhora.controller('viewPassbookCtrl', function($rootScope, $scope, TRANSACTION_TY
    $scope.propertyName = propertyName;
  };
  
+ $scope.editTransaction = (transaction)=>{
+   //TODO
+   $rootScope.editModeData = transaction;
+   $rootScope.template = {title: 'Edit Transaction', content :'transaction/updateTransaction.html'};
+ };
+ 
+ $scope.deleteTransaction = (transaction)=>{
+   shell.beep()
+   dialog.showMessageBox({
+       type: 'question',
+       buttons: ['Yes', 'No'],
+       title: 'Confirm',
+       message: `Are you sure you want to delete ${transaction.name}'s transaction'?`
+   }, function (response) {
+       if (response === 0) {
+        let  {amount, date, promiseDate, type, customerId, name, village, remarks } = transaction;
+        let keys = ['amount', 'date', 'promiseDate', 'type', 'customerId', 'name', 'village', 'remarks' ];
+        let values =[amount, date, promiseDate, type, customerId, name, village, remarks];
+         q.insert(DELTRANSACTION_TABLE, keys, values)
+         .then((data)=>{
+           return q.deleteRowById(TRANSACTION_TABLE, transaction.id);
+         })
+         .then((data)=>{
+           $scope.getCustomerPassbook(TRANSACTION_TABLE);
+           dialog.showMessageBox({type :'info', message:`${transaction.name}'s transaction deleted`, buttons:[]});
+         })
+         .catch((err)=>{
+           console.error('anp an err occured while deleting', transaction);
+         });
+       }
+   })
+ };
+ 
  $scope.getCustomerPassbook = (tableName)=>{
       q.selectAllById(tableName, 'customerId', $rootScope.viewPassbookData.id)
       .then((rows)=>{
