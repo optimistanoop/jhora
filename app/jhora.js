01d34877c2c29e9868707e64faf21834a02a4654
optimistanoop <optimistanoop@gmail.com>
2018-05-30 23:57:19 +0530

    date formatter

--- app/jhora.js
+++ app/jhora.js
@@ -60,10 +60,36 @@ jhora.controller('jhoraCtrl', function($rootScope, $scope, VILLAGES, TABS) {
 //       requireBase: false
 //     });
 // });
-jhora.config(function($mdThemingProvider) {
+jhora.config(function($mdThemingProvider, $mdDateLocaleProvider) {
   $mdThemingProvider.theme('docs-dark', 'default').primaryPalette('yellow') .dark();
   $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
   $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
   $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
   $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+  
+  $mdDateLocaleProvider.parseDate = function(dateString) {
+    let dd = dateString ? new Date(dateString) : undefined;
+    let formattedDate = '';
+    if(dd){
+      let d = dd.getDate();
+      let m = dd.getMonth();
+      let y = dd.getFullYear();
+      formattedDate = `${d}-${m + 1}-${y}`
+    }
+    
+    return formattedDate ? formattedDate : undefined;
+  };
+  
+  $mdDateLocaleProvider.formatDate = function(date) {
+  let dd = date ? date : undefined;
+  let formattedDate = '';
+  if(dd){
+    let d = dd.getDate();
+    let m = dd.getMonth();
+    let y = dd.getFullYear();
+    formattedDate = `${d}-${m + 1}-${y}`
+  }
+  
+  return formattedDate ? formattedDate : undefined;
+};
 });
