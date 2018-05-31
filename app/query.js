673c336a9a9c90655d4737e777992d7a163f2d3a
Mahesh-maxx <mahesh.max.kumar86@gmail.com>
2018-05-31 16:42:33 +0530

    Error removed

--- app/query.js
+++ app/query.js
@@ -113,17 +113,15 @@ class Query {
     });
     return p;
   }
-<<<<<<< HEAD
+
   selectAllTransactionByDate(tableName, key, value1,value2){
     let p = new Promise( (resolve, reject)=>{
       let sql = `SELECT * FROM ${tableName} WHERE ${key} = BETWEEN toDate(${value1}) AND toDate(${value2})`
-=======
 
-  //get data by year and month of selected data 
+  //get data by year and month of selected data
    selectAllByYearMonth(tableName, key, value){
     let p = new Promise( (resolve, reject)=>{
       let sql = `SELECT * FROM ${tableName} WHERE strftime('%Y', ${key}) = strftime('%Y', ${key}) AND strftime('%m', ${key}) = strftime('%m', ${key})`
->>>>>>> dc077b2e48465c3434cde8c927ef3a26b03d6493
       this.db.all(sql, (err, data)=>{
         if(err) reject(err);
         resolve(data);
@@ -131,10 +129,8 @@ class Query {
     });
     return p;
   }
-<<<<<<< HEAD
-=======
 
-  //get data between two dates 
+  //get data between two dates
    selectDataByDates(tableName, key, value1,value2){
     let p = new Promise( (resolve, reject)=>{
       let sql = `SELECT * FROM ${tableName} WHERE date(${key}) BETWEEN date(${value1}) AND date(${value2})`
@@ -146,7 +142,7 @@ class Query {
     return p;
   }
 
-//get greater data and equal of selected date 
+//get greater data and equal of selected date
   selectGreaterDataByDate(tableName,key,value){
     let p = new Promise( (resolve, reject)=>{
     let sql = `SELECT * from ${tableName} where date(${key}) >= date(${value})`
@@ -158,7 +154,7 @@ class Query {
     return p;
   }
 
-  //get less data of selected date 
+  //get less data of selected date
   selectLessDataByDate(tableName,key,value){
     let p = new Promise( (resolve, reject)=>{
     let sql = `SELECT * from ${tableName} where date(${key}) < date(${value})`
@@ -170,7 +166,7 @@ class Query {
     return p;
   }
 
->>>>>>> dc077b2e48465c3434cde8c927ef3a26b03d6493
+
 };
 
 module.exports = Query;
