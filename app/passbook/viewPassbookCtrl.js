279c51e88763614e6574d7b570ea810dfe9d5ba9
Mahesh-maxx <mahesh.max.kumar86@gmail.com>
2018-06-10 01:07:35 +0530

    Routing fixed on add & update transaction passbook link

--- app/passbook/viewPassbookCtrl.js
+++ app/passbook/viewPassbookCtrl.js
@@ -3,158 +3,160 @@ jhora.controller('viewPassbookCtrl', function($rootScope, $scope, $timeout, $rou
 
   const {dialog} = require('electron').remote;
   const {shell} = require('electron');
+  $rootScope.template = {title: 'Passbook'};
   $scope.custid=$routeParams.id;
   $scope.customer = {};
   $scope.init = ()=> {
     q.selectAllById('customers', 'id', $scope.custid)
     .then((rows)=>{
       $timeout(()=> {
-      $scope.customer = rows[0];
-  $scope.hideNoDataFound = true;
-  $scope.salutation = '';
-      if($scope.customer.salutation == 'Mrs'){
+        $scope.customer = rows[0];
+        $rootScope.template.title=`${$scope.customer.name}'s Passbook`;
+        $scope.hideNoDataFound = true;
+        $scope.salutation = '';
+        if($scope.customer.salutation == 'Mrs'){
           $scope.salutation = 'W/o' ;
-      }else if($scope.customer.salutation == 'Mr'){
+        }else if($scope.customer.salutation == 'Mr'){
           $scope.salutation = 'S/o' ;
-      }else{
+        }else{
           $scope.salutation = 'D/o' ;
         }
- $scope.sortBy = (propertyName)=>{
-   $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-   $scope.propertyName = propertyName;
- };
+        $scope.sortBy = (propertyName)=>{
+          $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+          $scope.propertyName = propertyName;
+        };
 
- $scope.deleteTransaction=(ev,transaction)=>{
-   shell.beep()
-   $mdDialog.show({
-   controller: ($scope, $mdDialog)=>{
-   $scope.message = 'Are you sure to delete...?'
-   $scope.transaction = transaction;
-   $scope.answer = function(answer) {
-   $mdDialog.hide(answer);
-  };
-},
-  templateUrl: 'transaction/previewTransaction.html',
-  parent: angular.element(document.body),
-  targetEvent: ev,
-  clickOutsideToClose:false,
-  fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-})
-.then(function(answer) {
-  if(answer == 'submit') {
-    $scope.confirmTransaction(transaction);
-  }
-});
-}
- $scope.confirmTransaction = (transaction)=>{
-        let  {amount, rate, date, promiseDate, type, customerId, name, village, remarks } = transaction;
-        let keys = ['amount', 'rate', 'date', 'promiseDate', 'type', 'customerId', 'name', 'village', 'remarks' ];
-        let values =[amount,rate, date, promiseDate, type, customerId, name, village, remarks];
-         q.insert(DELTRANSACTION_TABLE, keys, values)
-         .then((data)=>{
-           return q.deleteRowById(TRANSACTION_TABLE, transaction.id);
-         })
-         .then((data)=>{
-           $scope.getDataByTable(TRANSACTION_TABLE, TRANSACTION_TABLE);
-           $mdToast.show(
-           $mdToast.simple()
-           .textContent(`${transaction.name}'s Transaction Deleted.`)
-           .position('bottom right')
-           .hideDelay(3000)
-           );
-         })
-         .catch((err)=>{
-           console.error('anp an err occured while deleting',err);
-         });
-       }
-       $scope.getDataByTable = (tableName, modelName)=>{
-         q.selectAll(tableName)
-         .then((rows)=>{
-           if(rows)
-           for(let row of rows){
-             row.date = row.date ? new Date(row.date) : null;
-             if(tableName == TRANSACTION_TABLE || tableName == DELTRANSACTION_TABLE)
-             row.promiseDate = row.promiseDate ? new Date(row.promiseDate) : null;
-           }
-           $timeout(()=>{
-             // $scope[modelName] = rows;
-             $scope.transactions= rows;
-             // console.log('data',$scope[modelName]);
-             $scope.hideNoDataFound = true;
-             if(tableName == TRANSACTION_TABLE && rows && rows.length == 0)
-             $scope.hideNoDataFound = false;
-           }, 0);
-         })
-         .catch((err)=>{
-           console.error('anp got error while fetching data',err);
-         });
-       };
-
-       
- $scope.getCustomerPassbook = (tableName)=>{
-      q.selectAllById(tableName, 'customerId', $scope.custid)
-      .then((rows)=>{
-        if(rows)
-        for(let row of rows){
-          row.date = row.date ? new Date(row.date) : null;
-          row.promiseDate = row.promiseDate ? new Date(row.promiseDate) : null;
+        $scope.deleteTransaction=(ev,transaction)=>{
+          shell.beep()
+          $mdDialog.show({
+            controller: ($scope, $mdDialog)=>{
+              $scope.message = 'Are you sure to delete...?'
+              $scope.transaction = transaction;
+              $scope.answer = function(answer) {
+                $mdDialog.hide(answer);
+              };
+            },
+            templateUrl: 'transaction/previewTransaction.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:false,
+            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+          })
+          .then(function(answer) {
+            if(answer == 'submit') {
+              $scope.confirmTransaction(transaction);
+            }
+          });
         }
-        $timeout(()=>{
-          $scope.transactions = rows;
-          if(tableName == TRANSACTION_TABLE && rows && rows.length == 0)
-          $scope.hideNoDataFound = false;
-        },0);
-      })
-      .catch((err)=>{
-        console.error(err);
-      });
+        $scope.confirmTransaction = (transaction)=>{
+          let  {amount, rate, date, promiseDate, type, customerId, name, village, remarks } = transaction;
+          let keys = ['amount', 'rate', 'date', 'promiseDate', 'type', 'customerId', 'name', 'village', 'remarks' ];
+          let values =[amount,rate, date, promiseDate, type, customerId, name, village, remarks];
+          q.insert(DELTRANSACTION_TABLE, keys, values)
+          .then((data)=>{
+            return q.deleteRowById(TRANSACTION_TABLE, transaction.id);
+          })
+          .then((data)=>{
+            $scope.getDataByTable(TRANSACTION_TABLE, TRANSACTION_TABLE);
+            $mdToast.show(
+              $mdToast.simple()
+              .textContent(`${transaction.name}'s Transaction Deleted.`)
+              .position('bottom right')
+              .hideDelay(3000)
+            );
+          })
+          .catch((err)=>{
+            console.error('anp an err occured while deleting',err);
+          });
+        }
+        $scope.getDataByTable = (tableName, modelName)=>{
+          q.selectAll(tableName)
+          .then((rows)=>{
+            if(rows)
+            for(let row of rows){
+              row.date = row.date ? new Date(row.date) : null;
+              if(tableName == TRANSACTION_TABLE || tableName == DELTRANSACTION_TABLE)
+              row.promiseDate = row.promiseDate ? new Date(row.promiseDate) : null;
+            }
+            $timeout(()=>{
+              // $scope[modelName] = rows;
+              $scope.transactions= rows;
+              // console.log('data',$scope[modelName]);
+              $scope.hideNoDataFound = true;
+              if(tableName == TRANSACTION_TABLE && rows && rows.length == 0)
+              $scope.hideNoDataFound = false;
+            }, 0);
+          })
+          .catch((err)=>{
+            console.error('anp got error while fetching data',err);
+          });
+        };
+
+
+        $scope.getCustomerPassbook = (tableName)=>{
+          q.selectAllById(tableName, 'customerId', $scope.custid)
+          .then((rows)=>{
+            if(rows)
+            for(let row of rows){
+              row.date = row.date ? new Date(row.date) : null;
+              row.promiseDate = row.promiseDate ? new Date(row.promiseDate) : null;
+            }
+            $timeout(()=>{
+              $scope.transactions = rows;
+              if(tableName == TRANSACTION_TABLE && rows && rows.length == 0)
+              $scope.hideNoDataFound = false;
+            },0);
+          })
+          .catch((err)=>{
+            console.error(err);
+          });
+        };
+
+        $scope.getCustomerPassbook(TRANSACTION_TABLE);
+        $scope.Back = ()=>{
+          $window.history.back();
+        }
+        let getMonthDiff2 = (from, to)=>{
+
+          from = new Date(from);
+          to = new Date(to);
+          let valid = !isNaN(from) && !isNaN(to) && from < to ;
+          if(! valid) return [];
+          let months = 0;
+          let firstMonth = 0;
+          let lastMonth = 0;
+          months = (to.getFullYear() - from.getFullYear()) * 12;
+          months -= from.getMonth() + 1;
+          months += to.getMonth();
+          months = months <= 0 ? 0 : months;
+
+          firstMonth = from.getDate() <= 15 ? 1 :0.5;
+          lastMonth = to.getDate() >= 15 ? 1 :0.5;
+          return [firstMonth, months, lastMonth];
+        }
+
+
+        function getMonthDiff (from, to) {
+          from = new Date(from);
+          to = new Date(to);
+          let valid = !isNaN(from) && !isNaN(to) && from < to ;
+          if(! valid) return [];
+          let months = 0;
+          let firstMonth = 0;
+          let lastMonth = 0;
+          months = (to.getFullYear() - from.getFullYear()) * 12;
+          months -= from.getMonth() + 1;
+          months += to.getMonth();
+          months = months <= 0 ? 0 : months;
+
+          firstMonth = from.getDate() <= 15 ? 1 :0.5;
+          lastMonth = to.getDate() >= 15 ? 1 :0.5;
+          return [firstMonth, months, lastMonth];
+        }
+      },0)
+    })
   };
-
-  $scope.getCustomerPassbook(TRANSACTION_TABLE);
-  $scope.Back = ()=>{
-    $window.history.back();
-  }
-  let getMonthDiff2 = (from, to)=>{
-
-    from = new Date(from);
-    to = new Date(to);
-    let valid = !isNaN(from) && !isNaN(to) && from < to ;
-    if(! valid) return [];
-    let months = 0;
-    let firstMonth = 0;
-    let lastMonth = 0;
-    months = (to.getFullYear() - from.getFullYear()) * 12;
-    months -= from.getMonth() + 1;
-    months += to.getMonth();
-    months = months <= 0 ? 0 : months;
-
-    firstMonth = from.getDate() <= 15 ? 1 :0.5;
-    lastMonth = to.getDate() >= 15 ? 1 :0.5;
-    return [firstMonth, months, lastMonth];
-  }
-
-
-  function getMonthDiff (from, to) {
-    from = new Date(from);
-    to = new Date(to);
-    let valid = !isNaN(from) && !isNaN(to) && from < to ;
-    if(! valid) return [];
-    let months = 0;
-    let firstMonth = 0;
-    let lastMonth = 0;
-    months = (to.getFullYear() - from.getFullYear()) * 12;
-    months -= from.getMonth() + 1;
-    months += to.getMonth();
-    months = months <= 0 ? 0 : months;
-
-    firstMonth = from.getDate() <= 15 ? 1 :0.5;
-    lastMonth = to.getDate() >= 15 ? 1 :0.5;
-    return [firstMonth, months, lastMonth];
-  }
-},0)
-})
-};
-$scope.init();
+  $scope.init();
   let transactions = [
     { amount: 100, date: '2018-01-01', promiseDate: '2018-11-11', type: 'Dr', customerId: '1', name: 'Anoop', village:'Daniyari', remarks: '' },
     { amount: 500, date: '2018-01-01', promiseDate: '2018-11-11', type: 'Dr', customerId: '1', name: 'Anoop', village:'Daniyari', remarks: '' },
@@ -166,4 +168,4 @@ $scope.init();
   ];
 
 
-  });
+});
